17c63169cb1204d712625dbcdb7bdcac80e10541
Nilan <nilan.marktanner@student.kit.edu>
2017-03-26 00:12:22 +0100

    single quotes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ export default class Footer extends Component {
   render() {
     return (
       <div className='Footer'>
-        Built with ❤︎ by <a href="https://twitter.com/_marktani">Nilan</a> using <a href='https://graph.cool'>Graphcool</a> and <a href='https://netlify.com'>Netlify</a> <br/>
-        Inspired by <a href="http://www.chaiandconversation.com/">Chai and Conversation</a>
+        Built with ❤︎ by <a href='https://twitter.com/_marktani'>Nilan</a> using <a href='https://graph.cool'>Graphcool</a> and <a href='https://netlify.com'>Netlify</a> <br/>
+        Inspired by <a href='http://www.chaiandconversation.com/'>Chai and Conversation</a>
       </div>
     )
   }
